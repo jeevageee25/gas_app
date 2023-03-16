be6720a8936b1ae527568dba922b926293b64c74
Jeeva Ganesan <j.ganesh@dfarm.in>
2023-03-16 22:11:07 +0530

    Sales reprot changes

--- src/app/home/report/sales-report/sales-report.component.ts
+++ src/app/home/report/sales-report/sales-report.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Config, DefaultConfig, Columns } from 'ngx-easy-table';
+import { DialogService } from 'primeng/dynamicdialog';
+import { combineLatest } from 'rxjs';
+import { GlobalService } from 'src/app/services/global.service';
+import { ProductsService } from 'src/app/services/products.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { PaymentDetailComponent } from '../payment-detail/payment-detail.component';
 
 @Component({
   selector: 'app-sales-report',
@@ -7,9 +15,155 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesReportComponent implements OnInit {
 
-  constructor() { }
+  dateForm: FormGroup = new FormGroup({});
+  configuration: Config = { ...DefaultConfig };
+  columns: Columns[] = [];
+  tableData = [{}];
+  areaObj: any = {};
+  productObj: any = {};
+  executiveObj: any = {};
+
+  constructor(private fb: FormBuilder, private PService: ProductsService,
+    private toastService: ToastService, private gs: GlobalService, public dialogService: DialogService) { }
 
   ngOnInit(): void {
+    this.createForm();
+    this.initTable();
+    this.initConfig();
+    this.searchProducts();
+    this.getData();
+  }
+
+
+  createForm() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0)
+    this.dateForm = this.fb.group({
+      allocation_date: [date, Validators.required]
+    })
+  }
+
+  initTable() {
+    this.columns = [
+      { key: '', title: 'SL #' },
+      { key: 'date', title: 'Date' },
+      { key: 'expected', title: 'Expected Amount' },
+      { key: 'collected', title: 'Collected Amount' },
+      { key: 'credit', title: 'Credit' },
+      { key: 'difference', title: 'Difference' },
+    ];
+  }
+
+  initConfig() {
+    this.configuration = { ...DefaultConfig };
+    this.configuration.detailsTemplate = true;
+    this.configuration.fixedColumnWidth = false;
+    this.configuration.rows = 10;
+  }
+
+  convertDateFormat(date: any) {
+    const d = new Date(date);
+    return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+  }
+
+  getAreas() {
+    this.PService.searchArea({ search_key: {} }).subscribe((res: any) => {
+      (res?.data || []).map((i: any) => {
+        this.areaObj[i._id] = i.area;
+      })
+    }, e => {
+      this.toastService.showErrorToaster('Error', 'Something went wrong !. Please try again later.');
+    })
+  }
+
+  searchProducts() {
+    this.PService.getProducts({ search_key: {} }).subscribe((res: any) => {
+      (res?.data || []).map((i: any) => {
+        this.productObj[i._id] = `${i.category} - ${i.name}`;
+      })
+    }, e => {
+      this.toastService.showErrorToaster('Error', 'Something went wrong !. Please try again later.');
+    })
+  }
+
+  getExecutives() {
+    this.PService.searchEmployee({ search_key: { role: "Delivery Executive" } }).subscribe((res: any) => {
+      (res?.data || [])?.map((i: any) => {
+        this.executiveObj[i._id] = i.staff_name;
+      })
+    }, e => {
+      this.toastService.showErrorToaster('Error', 'Something went wrong !. Please try again later.');
+    })
+  }
+
+  getData() {
+    const date = new Date(this.dateForm?.get('allocation_date')?.value);
+    date.setHours(0, 0, 0, 0);
+    const allocation_date = this.convertDateFormat(date);
+    combineLatest(
+      this.PService.searchSales({ search_key: {} }),
+      this.PService.searchAreaAllocation({ search_key: {} })
+    ).subscribe((res: any) => {
+      const sales_entry = res[0]?.data || [];
+      const allocation_data = res[1]?.data || [];
+      const a_grouped = this.gs.groupBy(allocation_data, ['allocation_date']);
+      const s_grouped: any = this.gs.groupBy(sales_entry, ['allocation_date']);
+      let tableData = [];
+
+      for (const [date, value] of Object.entries(a_grouped)) {
+        let expected = 0;
+        let data: any = value;
+        data.forEach((d: any) => {
+          d.allocation_data.forEach((a: any) => {
+            a.allocations.forEach((s: any) => {
+              expected += (s.count * s.price);
+            })
+          })
+        })
+        const { credit, collected } = this.formatReports(s_grouped[date] || [])
+        const difference = Number((credit+collected-expected).toFixed(2));
+        tableData.push({ date, credit:Number(credit.toFixed(2)), collected:Number(collected.toFixed(2)), expected:Number(expected.toFixed(2)), difference })
+      }
+      this.tableData = tableData;
+    }, e => {
+      this.toastService.showErrorToaster('Error', 'Something went wrong !. Please try again later.');
+    })
+  }
+
+  formatReports(sales_list: any) {
+    let collected = 0;
+    let credit = 0;
+    sales_list.forEach((sales: any) => {
+      if (sales?.paymentMode === 'cash') {
+        for (const [c, v] of Object.entries(sales?.payments)) {
+          let a: any = c;
+          let b: any = v;
+          if (v != 0) {
+            collected += (a * b);
+          }
+        }
+      }
+      else if (sales?.paymentMode === 'credit') {
+        collected += sales.supplied * sales.price;
+        credit += sales.supplied * sales.price;
+      }
+      else {
+        collected += sales.supplied * sales.price;
+      }
+    })
+    return { credit, collected }
+  }
+
+  openPayment(row: any) {
+    this.dialogService.open(PaymentDetailComponent, {
+      header: 'Payment Details',
+      width: '40%',
+      data: row.sales
+    });
+  }
+
+  get areas() {
+    return Object.keys(this.tableData)
   }
 
 }
